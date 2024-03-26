86ff8c28dced71b9a5f2f4f5cea2d9289ac7a05b
hy4ri <hiari45@gmail.com>
2024-03-26 23:58:47 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// Get a reference to your checkbox element (replace "myCheckbox" with the actual ID)
+const myCheckbox = document.getElementById("darks");
+
+// Add an event listener for the "change" event
+myCheckbox.addEventListener("change", function () {
+  // This function will be executed whenever the checkbox state changes
+
+  // Check the current state of the checkbox
+  if (myCheckbox.checked) {
+    document.body.style.backgroundColor = "black";
+    document.getElementById("main").style.color = "white";
+    document.getElementById("aside").style.borderColor = "White";
+    document.getElementById("navbar").style.backgroundColor = "#17696c";
+    document.getElementById("darkss").textContent = "🌙";
+  } else {
+    document.body.style.backgroundColor = "#ebdf96";
+    document.getElementById("main").style.color = "black";
+    document.getElementById("aside").style.borderColor = "black";
+    document.getElementById("darkss").textContent = "☀️";
+  }
+});
